ada5e7d0ea9fb4b07064e5fdc13889b759600ebc
Lin Yang(Search) <lnyang@linkedin.com>
2015-05-05 19:07:54 -0700

    test

--- test_utils.py
+++ test_utils.py
@@ -13,3 +13,57 @@ def test_getNextYm():
   assert utils.getNextYm('1984-02', 11) == '1985-01'
   assert utils.getNextYm('1984-02', 123) == '1994-05'
 
+def test_updateCountDict():
+  d = dict()
+  utils.updateCountDict(d, 'key1')
+  assert d == {'key1': 1}
+  utils.updateCountDict(d, 'key1')
+  assert d == {'key1': 2}
+  utils.updateCountDict(d, 'key2')
+  assert d == {'key1': 2, 'key2': 1}
+
+def test_updateListDict():
+  d = dict()
+  utils.updateListDict(d, 'key1', 1)
+  assert d == {'key1': [1]}
+  utils.updateListDict(d, 'key1', 2)
+  assert d == {'key1': [1, 2]}
+  utils.updateListDict(d, 'key2', 3)
+  assert d == {'key1': [1, 2], 'key2': [3]}
+
+def test_readL1():
+  lines = (
+      'date\tABC\tXYZ',
+      '1984-02-22\t1\t2',
+      '2000-01-01\t\t',
+      '2015-05-05\t\t-123')
+  data = utils.readL1(lines)
+  assert data == {
+      '1984-02-22': {'ABC': 1.0, 'XYZ': 2.0},
+      '2000-01-01': {},
+      '2015-05-05': {'XYZ': -123.0}}
+
+def test_computeFeatureStats():
+  features = [('1984', None),
+              ('1984', 1),
+              ('1984', 3),
+              ('1984', 5),
+              ('1984', 7),
+              ('1984', 9),
+              ('1984', None),
+              ('1984', 10),
+              ('1984', 8),
+              ('1984', 6),
+              ('1984', 4),
+              ('1984', 2),
+              ('1984', None),
+              ('2015', 100),
+              ('2000', None),
+              ('2000', None)]
+  stats = utils.computeFeatureStats(features)
+  assert stats == {
+      '1984': [10, 13, 5, 1, 1, 2, 3, 6, 8, 10, 10, 10],
+      '2000': [0, 2, None, None, None, None, None, None,
+               None, None, None, None],
+      '2015': [1, 1, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]}
+
--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ def configLogging(level=logging.INFO):
   logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s',
                       level=level)
 
+def getY(ymd):
+  y, m, d = ymd.split('-')
+  return y
+
 def getYm(ymd):
   y, m, d = ymd.split('-')
   return '%s-%s' % (y, m)
@@ -19,4 +23,61 @@ def getNextYm(ym, k=1):
     m -= 12
     y += 1
   return '%02d-%02d' % (y, m)
+
+def updateCountDict(count_dict, key):
+  if key not in count_dict: count_dict[key] = 1
+  else: count_dict[key] += 1
+
+def updateListDict(list_dict, key, value):
+  if key not in list_dict: list_dict[key] = [value]
+  else: list_dict[key].append(value)
+
+def readL1(lines):
+  """ Reads l1 file into a dict: date => {indicator: value}.
+  """
+  assert len(lines) > 0
+  headers = lines[0].split('\t')
+  assert len(headers) > 0
+  assert all([header != '' for header in headers])
+  assert headers[0] == 'date'
+  data = dict()
+  for i in range(1, len(lines)):
+    items = lines[i].split('\t')
+    assert len(items) == len(headers)
+    row = dict()
+    for j in range(1, len(headers)):
+      if items[j] != '':
+        row[headers[j]] = float(items[j])
+    data[items[0]] = row
+  return data
+
+def computeFeatureStats(features):
+  """ Computes feature stats.
+      features: [(y, f), (y, f), ...] where f is either float or None
+                indicating nonexistent feature value.
+      returns: dict of y => [count, total, avg, min, 1p, 10p, 25p, 50p,
+                             75p, 90p, 99p, max]
+      Feature coverage = count / total, eg.
+  """
+  ncounts = dict()  # y => #None
+  values = dict()  # y => [non-None values]
+  for y, f in features:
+    if f is None:
+      updateCountDict(ncounts, y)
+    else:
+      updateListDict(values, y, f)
+  stats = dict()
+  for y, v in values.iteritems():
+    v.sort()
+    lenv = len(v)
+    stats[y] = [lenv, lenv + ncounts.get(y, 0), sum(v)/lenv, v[0],
+                v[int(lenv*0.01)], v[int(lenv*0.1)],
+                v[int(lenv*0.25)], v[int(lenv*0.5)],
+                v[int(lenv*0.75)], v[int(lenv*0.9)],
+                v[int(lenv*0.99)], v[-1]]
+  for y, n in ncounts.iteritems():
+    if y not in stats:
+      stats[y] = [0, n, None, None, None, None, None, None, None,
+                  None, None, None]
+  return stats
 
